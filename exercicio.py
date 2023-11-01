917d55af58c491e416bd53b2d2ddbbe40f6c5f38
Felipe <sishigame2005@gmail.com>
2023-11-01 15:05:09 -0300

    codigo py

---
+++ exercicio.py
@@ -0,0 +1,14 @@
+print("Exercicios de GitHub")
+
+
+nome = input("Digite o seu nome:")
+
+note1 = input("Coloque uma nota:")
+note2 = input("Coloque uma segunda nota:")
+note3 = input("Coloque terceira nota:")
+note4= input("Coloque quarta nota:")
+
+media = (note1 +note2 +  note3 + note4 /4)
+
+
+print(F"O aluno:{nome} \nObteve a média: {media}")
